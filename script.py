3e7e507860bbf4cbbf0482f04a4fa760190ee7ca
michel_boissac <101632678+Michelboissac@users.noreply.github.com>
2025-09-08 10:38:48 +0200

    Update script.py

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ param2 = st.text_input("dossier genome (ou telecharger)", placeholder="telecharg
 param3 = st.text_input("extension", placeholder=".fastq")
 param4 = st.text_input("dossier reads (ou telecharger)", placeholder="telecharger OU /mnt/c/etc/dossier_travail/reads/")
 param5 = st.text_input("nom base projet", placeholder="PRJ_xxxxxxx_Ovaries    > créer un sous dossier : /mnt/c/etc/dossier_travail/PRJ_xxxxxxx_Ovaries/")
-param6 = st.text_input("liste SRA", placeholder="laisser vide si on telecharge les SRA, sinon :\"SRRXXXXXX1 SRRXXXXXX2\" metre \"\" autour")
+param6 = st.text_input("liste SRA", placeholder="laisser \"\" si on telecharge les SRA, sinon :\"SRRXXXXXX1 SRRXXXXXX2\" metre \"\" autour")
 param7 = st.text_input("technologie de séquençage short long", placeholder="short ou long")
 param8 = st.text_input("single or pair end ?", placeholder="single ou pair")
 
@@ -18,7 +18,7 @@ param8 = st.text_input("single or pair end ?", placeholder="single ou pair")
 
 
 if st.button("Launch Analysis"):
-    cmd = f"bash alignement.sh {param1} {param2} {param3} {param4} {param5} {param6} {param7} {param7}"
+    cmd = f"bash alignement.sh {param1} {param2} {param3} {param4} {param5} {param6} {param7} {param8}"
     try:
         result = subprocess.run(
             cmd,
@@ -42,4 +42,5 @@ import webbrowser
 url = "http://localhost:8501"
 
 # Ouvre le navigateur par défaut
+
 webbrowser.open(url)
